116b334050e7fda19cc38aadb72345ed3060b75c
Daniel Ogunyemi <ogunyemidaniel65@gmail.com>
2023-09-07 16:47:00 -0700

    My commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
+
+
+/**
+ * main - Prints the string in the printf function.
+ * Return: Has a value of 0 to stop program.
+ */
+
+
 int main(void) 
 {
 	printf("with proper grammar, but the outcome is a piece of art,");
