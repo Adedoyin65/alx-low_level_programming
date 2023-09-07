0312994b19f16aa16c40fdf4549fba8c3dd12db7
Daniel Ogunyemi <ogunyemidaniel65@gmail.com>
2023-09-07 15:19:40 -0700

    My commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
 
-/**
- * main - prints "Programming is like building a multilingual 
- * puzzle,followed by a new line.
- * Return:(0).
- */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
