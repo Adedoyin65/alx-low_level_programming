1d92c4e95d5d6d369a426e44973c1fa28a2ec500
Daniel Ogunyemi <ogunyemidaniel65@gmail.com>
2023-09-08 16:19:09 -0700

    Love c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,5 +16,11 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	printf("%d is positive\n", n);
+	if (n == 0) {
+		printf("%d is zero\n", n);
+	}
+	else {
+		printf("%d is negative\n", n);
+	}
 	return (0);
 }
