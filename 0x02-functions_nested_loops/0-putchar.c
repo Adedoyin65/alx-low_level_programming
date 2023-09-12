37960b45f30ab541edbcb9266b26e06277df7c3b
Daniel Ogunyemi <ogunyemidaniel65@gmail.com>
2023-09-12 10:21:30 -0700

    Loving c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,1 @@
 #include <stdio.h>
-
-
-/**
- * main - A program that prints _putchar.
- * Return: Always 0.
- */
-
-int main(void)
-{
-	char a = '_', b = 'p', c ='u', d = 't', e = 'c', f = 'h', 
-	     g = 'a', h = 'r';
-	
-	putchar("%c%c%c%c%c%c%c%c\n");
-	return (0);
-}
---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * _putchar - A program that prints _putchar.
+ * Return: Always 0.
+ */
+
+
+int main()
+{;
+	
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
