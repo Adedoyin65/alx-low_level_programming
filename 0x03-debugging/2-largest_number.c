e7a0362f6f402e5a32e7ebc1b660e14fbf5e435c
Daniel Ogunyemi <ogunyemidaniel65@gmail.com>
2023-09-14 12:12:27 -0700

    Love c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -17,13 +17,13 @@ int largest_number(int a, int b, int c)
 	{
 		largest = a;
 	}
-	else if (b > a && a > c)
+	else if (c > b && a > b && c > a)
 	{
-		largest = b;
+		largest = c;
 	}
 	else
 	{
-		largest = c;
+		largest = b;
 	}
 
 	return (largest);
