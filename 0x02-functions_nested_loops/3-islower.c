4c3b8b334cd74b95dc6666869635632c17f18b45
Daniel Ogunyemi <ogunyemidaniel65@gmail.com>
2023-09-13 23:26:35 -0700

    Love c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,6 +3,7 @@
 
 /**
  * _islower(int c) - A function that checks for lowercase.
+ * islower(c) - returns 1 for true, 0 for false.
  * RETURN: 1 for success, 0 for failure.
  */
 
@@ -11,7 +12,7 @@ int _islower(int c)
 {
 
 
-	if(islower(c))
+	if (islower(c))
 	{
 		return (1);
 	}
