cf6d903b5eb6dcc5c049ffe71f6a6d276a9942d7
Daniel Ogunyemi <ogunyemidaniel65@gmail.com>
2023-09-14 17:15:58 -0700

    Love c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,7 @@
 #include <ctype.h>
 
 /**
- * int _isupper(int c) - function that checks for uppercase.
+ * _isupper - function that checks for uppercase.
  * @c: The number to check.
  * Return: 1 for success, 0 for failure
  */
