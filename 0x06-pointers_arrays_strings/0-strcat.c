4f230f8a63ec67c7affb14ed6a784b7591e4dff5
Daniel Ogunyemi <ogunyemidaniel65@gmail.com>
2023-09-21 05:44:30 -0700

    Love c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+
+/**
+ * _strcat - Write a function that concatenates two strings.
+ * Return: return dest.
+ */
+
+char *_strcat(char *dest, char *src)
+{
+
+	printf("%c\n",*dest + *src);
+	return dest;
+}
