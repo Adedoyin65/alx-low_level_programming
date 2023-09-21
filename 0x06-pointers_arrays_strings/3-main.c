60173b6a7532a7ec1181b95151195992f39d5510
Daniel Ogunyemi <ogunyemidaniel65@gmail.com>
2023-09-21 15:11:00 -0700

    Love c

--- 0x06-pointers_arrays_strings/3-main.c
+++ 0x06-pointers_arrays_strings/3-main.c
@@ -3,7 +3,7 @@
 
 /**
  * main - check the code
- * 
+ *
  * Return: Always 0.
  */
 int main(void)
@@ -14,5 +14,5 @@ int main(void)
 	    printf("%d\n", _strcmp(s1, s2));
 	    printf("%d\n", _strcmp(s2, s1));
 	    printf("%d\n", _strcmp(s1, s1));
-	    return (0);
+return (0);
 }
