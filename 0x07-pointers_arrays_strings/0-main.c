d818d5c66957069174530e354c4eb4ccb334c53d
Daniel Ogunyemi <ogunyemidaniel65@gmail.com>
2023-09-25 15:30:10 -0700

    Love c

--- 0x07-pointers_arrays_strings/0-main.c
+++ 0x07-pointers_arrays_strings/0-main.c
@@ -5,12 +5,12 @@
  * simple_print_buffer - prints buffer in hexa
  * @buffer: the address of memory to print
  * @size: the size of the memory to print
- * 
+ *
  * Return: Nothing.
  */
 void simple_print_buffer(char *buffer, unsigned int size)
 {
-	        unsigned int i;
+	unsigned int i;
 
 		i = 0;
 		while (i < size)
@@ -31,7 +31,7 @@ void simple_print_buffer(char *buffer, unsigned int size)
 
 /**
  * main - check the code
- * 
+ *
  * Return: Always 0.
  */
 int main(void)
@@ -42,5 +42,5 @@ int main(void)
 	    _memset(buffer, 0x01, 95);
 	    printf("-------------------------------------------------\n");
 	    simple_print_buffer(buffer, 98);
-	    return (0);
+return (0);
 }
