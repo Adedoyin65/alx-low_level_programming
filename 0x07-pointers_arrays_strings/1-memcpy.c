4c1efc6ff513f2dc37d68065f8bd9c1bf090e6e4
Daniel Ogunyemi <ogunyemidaniel65@gmail.com>
2023-09-25 16:26:16 -0700

    Love c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,12 +10,9 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	char *dest_ptr = dest;
-	char *src_ptr = src;
+	unsigned int i;
 
-	for (unsigned int i = 0; i < n; i++)
-	{
-		dest_ptr[i] = src_ptr[i];
-	}
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
 	return (dest);
 }
