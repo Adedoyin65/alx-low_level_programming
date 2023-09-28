2a1fc96c2d5700ae69a2150267495c92399a30f7
Daniel Ogunyemi <ogunyemidaniel65@gmail.com>
2023-09-28 07:27:27 -0700

    Love c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * _puts_recursion - A function that prints a string.
+ * @s: Input string.
+ * Return: Nothing.
+ */
+
+
+void _puts_recursion(char *s)
+{
+
+
+	puts(s);
+}
