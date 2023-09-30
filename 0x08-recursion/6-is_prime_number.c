cc7229609eac59efdfd73833e6f4754f0c17ba35
Daniel Ogunyemi <ogunyemidaniel65@gmail.com>
2023-09-29 17:33:23 -0700

    Love c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,5 +1,29 @@
 #include <stdio.h>
 
+/**
+ * is_divisible - Calculate the prime number.
+ * @n: Input integer.
+ * @divisor: To divide n.
+ * Return: is_divisible.
+ */
+
+int is_divisible(int n, int divisor)
+{
+	if (divisor == 1)
+	{
+		return (0);
+	}
+	if (divisor == 2)
+	{
+		return (n % 2 == 0) ? 1 : 0;
+	}
+	if (n % divisor == 0)
+	{
+		return (1);
+	}
+	return (is_divisible(n, divisor - 1));
+}
+
 
 /**
  * is_prime_number - A function that returns 1 if the input integer
@@ -10,33 +34,14 @@
 
 int is_prime_number(int n)
 {
-	int i, w;
-
 	if (n <= 1)
 	{
 		return (0);
 	}
-	if (n <= 3)
+	if (n == 2 || n == 3)
 	{
 		return (1);
 	}
-	if (n % 2 == 0 || n % 3 == 0)
-	{
-		return (0);
-	}
-
-	i = 5;
-	w = 2;
-
-	while (i * i <= n)
-	{
-		if (n % i == 0)
-		{
-			return (0);
-		}
-		i += w;
-		w = 6 - w;
-	}
-	return (1);
+	return (!is_divisible(n, n-1));
 
 }
