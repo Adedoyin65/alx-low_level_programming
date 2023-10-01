c0fb3ec6b89d471160bff88bff7bfe750abce454
Daniel Ogunyemi <ogunyemidaniel65@gmail.com>
2023-10-01 04:46:01 -0700

    Love c

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,30 @@
+#include <string.h>
+
+/**
+ * is_palindrome - Write a function that returns 1 if a string is 
+ * a palindrome and 0 if not.
+ * @s: Input string.
+ * Return: Either 1 or 0.
+ */
+
+int is_palindrome(char *s)
+{
+	int i, h;
+
+
+	i = 0;
+	h = strlen(s) - 1;
+
+	while (h > i)
+	{
+		if (s[i++] != s[h--])
+		{
+			return (0);
+		}
+		else if (s[i++] == s[h--])
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
