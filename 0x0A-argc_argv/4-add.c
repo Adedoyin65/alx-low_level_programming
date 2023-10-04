5e940f4e0cfec79b30cf4399a01166177857f92c
Daniel Ogunyemi <ogunyemidaniel65@gmail.com>
2023-10-04 12:05:45 -0700

    Love c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * main - Adds positive numbers.
+ * @argc: Argument count.
+ * @argv: Argument vector.
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, Result;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+	}
+
+	Result = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		int num = atoi(argv[i]);
+
+		if (num == 0 && argv[i][0] != '0')
+		{
+			printf("Error\n");
+			return (1);
+		}
+		Result += num;
+	}
+	printf("%d\n", Result);
+	return (0);
+}
+
+
