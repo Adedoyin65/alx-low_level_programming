acc626499f06f1c421bae069c3012d54d9c39b23
Daniel Ogunyemi <ogunyemidaniel65@gmail.com>
2023-10-11 10:22:58 -0700

    Love c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,13 +9,11 @@
 
 void print_dog(struct dog *d)
 {
-	if (d == NULL)
+	if (d != NULL)
 	{
-		return;
+		printf("%s\n", (d->name) ? d->name : "(nil)");
+		printf("%f\n", (d->age) ? d->age : 0);
+		printf("%s\n", (d->owner) ? d->owner : "(nil)");
 	}
-
-	printf("%s\n", d->name ? d->name : "(nil)");
-	printf("%.1f\n", d->age);
-	printf("%s\n", d->owner ? d->owner : "(nil)");
 }
 
