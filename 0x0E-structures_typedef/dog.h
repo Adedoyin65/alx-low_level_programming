0aa9eea37baa8fa34f018a2e3851929ba8b8792a
Daniel Ogunyemi <ogunyemidaniel65@gmail.com>
2023-10-11 09:52:55 -0700

    Love c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,17 +1,20 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
+
 /**
  * struct dog - Declares three variables
  * @name: name of dog.
  * @age: The age of dog.
  * @owner: The name of owner.
  */
+
 typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}dog_t;
+} dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
