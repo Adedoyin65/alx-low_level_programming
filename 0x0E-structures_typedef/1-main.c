5768d8762c8431ad9dffcc0a567fb5c2164f4177
Daniel Ogunyemi <ogunyemidaniel65@gmail.com>
2023-10-11 01:59:19 -0700

    Love c

--- 0x0E-structures_typedef/1-main.c
+++ 0x0E-structures_typedef/1-main.c
@@ -3,7 +3,7 @@
 
 /**
  * main - check the code
- * 
+ *
  * Return: Always 0.
  */
 int main(void)
@@ -11,6 +11,7 @@ int main(void)
 	    struct dog my_dog;
 
 	    init_dog(&my_dog, "Poppy", 3.5, "Bob");
-	    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
-	    return (0);
+	    printf("My name is %s, and I am %.1f :) - Woof!\n",
+			    my_dog.name, my_dog.age);
+return (0);
 }
