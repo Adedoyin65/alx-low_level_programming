0d544adc7683c54ef1b44d092709f1349e6d2a3a
Daniel Ogunyemi <ogunyemidaniel65@gmail.com>
2023-10-11 14:07:45 -0700

    Love c

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "dog.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * new_dog - Write a function that creates a new dog.
+ * @name: Name of dog.
+ * @age: Age of dog.
+ * @owner: Owner of the dog.
+ * Return: Nothing.
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *newdog = (dog_t *)malloc(sizeof(dog_t));
+
+	if (newdog == NULL)
+	{
+		return (NULL);
+	}
+	newdog->name = strdup(name);
+	newdog->owner = strdup(owner);
+
+	if (newdog->name == NULL || newdog->owner == NULL)
+	{
+		free (newdog->name);
+		free (newdog->owner);
+		free (newdog);
+		return (NULL);
+	}
+	newdog->age = age;
+	return (newdog);
+}
