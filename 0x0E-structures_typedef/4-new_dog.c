a3c0d7fa66c48a9b442858727633c6a8a1b0dcfa
Daniel Ogunyemi <ogunyemidaniel65@gmail.com>
2023-10-11 14:11:48 -0700

    Love c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -24,9 +24,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	if (newdog->name == NULL || newdog->owner == NULL)
 	{
-		free (newdog->name);
-		free (newdog->owner);
-		free (newdog);
+		free(newdog->name);
+		free(newdog->owner);
+		free(newdog);
 		return (NULL);
 	}
 	newdog->age = age;
