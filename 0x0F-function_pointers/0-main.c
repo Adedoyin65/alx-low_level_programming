0e570d1c663ba03c332a49efc1eff148067599c5
Daniel Ogunyemi <ogunyemidaniel65@gmail.com>
2023-10-11 22:46:21 -0700

    Love c

--- 0x0F-function_pointers/0-main.c
+++ 0x0F-function_pointers/0-main.c
@@ -4,7 +4,7 @@
 /**
  * print_name_as_is - prints a name as is
  * @name: name of the person
- * 
+ *
  * Return: Nothing.
  */
 void print_name_as_is(char *name)
@@ -15,7 +15,7 @@ void print_name_as_is(char *name)
 /**
  * print_name_uppercase - print a name in uppercase
  * @name: name of the person
- * 
+ *
  * Return: Nothing.
  */
 void print_name_uppercase(char *name)
@@ -24,23 +24,23 @@ void print_name_uppercase(char *name)
 
 	    printf("Hello, my uppercase name is ");
 	    i = 0;
-	    while (name[i])
-	    {
-		    if (name[i] >= 'a' && name[i] <= 'z')
-		    {
-			    putchar(name[i] + 'A' - 'a');
-		    }
-		    else
-		    {
-			    putchar(name[i]);
-		    }
-		    i++;
-	    }
+while (name[i])
+{
+if (name[i] >= 'a' && name[i] <= 'z')
+{
+	putchar(name[i] + 'A' - 'a');
+}
+else
+{
+	putchar(name[i]);
+}
+i++;
+}
 }
 
 /**
  * main - check the code
- * 
+ *
  * Return: Always 0.
  */
 int main(void)
@@ -48,5 +48,5 @@ int main(void)
 	    print_name("Bob", print_name_as_is);
 	    print_name("Bob Dylan", print_name_uppercase);
 	    printf("\n");
-	    return (0);
+return (0);
 }
