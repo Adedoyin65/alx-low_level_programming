eecd8cceef8f7347ef0a33d9417b8b3bdf170aa1
Daniel Ogunyemi <ogunyemidaniel65@gmail.com>
2023-10-14 22:12:12 -0700

    Love c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -9,43 +9,45 @@
 
 void print_all(const char * const format, ...)
 {
-	int i = 0;
-	int j = 0;
-	char *sep = ", ";
-
 	va_list p;
+	unsigned int i = 0, j, c = 0;
+	char *str;
+	const  char a[] = "cifs";
 
 	va_start(p, format);
-
-	while (format[i] != '\0')
+	while (format && format[i])
 	{
+		j = 0;
+		while (a[j])
+		{
+			if (format[i] == a[j] && c)
+			{
+				printf(", ");
+				break;
+			} j++;
+		}
 		switch (format[i])
 		{
 			case 'c':
-			printf("%c", va_arg(p, int));
-			break;
+				printf("%c", va_arg(p, int)), c = 1;
+				break;
 			case 'i':
-			printf("%d", va_arg(p, int));
-			break;
+				printf("%d", va_arg(p, int)), c = 1;
+				break;
 			case 'f':
-			printf("%f", va_arg(p, double));
-			break;
+				printf("%f", va_arg(p, double)), c = 1;
+				break;
 			case 's':
-			{
-			char *str = va_arg(p, char *);
-			if (str == NULL)
-			{
-			printf("(nil)");
-			}
-			printf("%s", str);
-			}
-			break;
-		}
-		if (j < i)
-		{
-		printf("%s", sep);
-		}
-		i++;
+				str = va_arg(p, char *), c = 1;
+				if (!str)
+				{
+					printf("(nil)");
+					break;
+				}
+				printf("%s", str);
+				break;
+		} i++;
 	}
+	va_end(p);
 	printf("\n");
 }
