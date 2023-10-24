6f583024f72de6c10dae6f46c25bc196507098d3
Ogunyemi Daniel <ogunyemidaniel65@gmail.com>
2023-10-24 04:16:43 -0700

    Love c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - A function that frees a listint_t list.
+ * @head: A pointer.
+ * Return: Nothing.
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *new;
+	
+	if (head == NULL)
+	{
+		while (*head != NULL)
+		{
+			new = *head;
+			*head = (*head)->next;
+			free(new);
+		}
+	}
+}
