46f331a3e2e9263065b5ceca344e453ba3066876
Ogunyemi Daniel <ogunyemidaniel65@gmail.com>
2023-11-03 10:29:18 -0700

    Love c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,27 +14,41 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t Bo, Br, Bw;
+	int fd;
+	ssize_t Bw, Br;
 	char *str;
 
 	if (filename == NULL)
 	{
 		return (0);
 	}
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		return (0);
+	}
 	str = malloc(sizeof(char) * letters);
 	if (str == NULL)
 	{
+		close(fd);
 		return (0);
 	}
-	Bo = open(filename, O_RDONLY);
-	Br = read(Bo, str, letters);
-	Bw = write(STDOUT_FILENO, str, Br);
-	if (Bo == -1 || Br == -1 || Bw == -1 || Bw != Br)
+	Br = read(fd, str, letters);
+	if (Br == -1)
 	{
+		close(fd);
 		free(str);
 		return (0);
 	}
+	Bw = write(STDOUT_FILENO, str, Br);
+	if (Bw == -1 || Bw != Br)
+	{
+		close(fd);
+		free(str);
+		return (0);
+	}
+	close(fd);
 	free(str);
-	close(Bo);
 	return (Bw);
+
 }
