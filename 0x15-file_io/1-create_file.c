882c8b6d383b8e6e2e92b9c4899d54df209e9254
Ogunyemi Daniel <ogunyemidaniel65@gmail.com>
2023-11-03 11:36:54 -0700

    Love c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+
+/**
+ * create_file - Create a function that creates a file.
+ * @filename: Is the name of the file to create.
+ * @text_content: Is a NULL terminated string to write to the file.
+ * Return: 1 on success, -1 on failure.
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t Bw;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+	if (text_content != NULL)
+	{
+		Bw = write(fd, text_content, strlen(text_content));
+		if (Bw == -1 || (size_t)Bw != strlen(text_content))
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+	close(fd);
+	return (1);
+}
