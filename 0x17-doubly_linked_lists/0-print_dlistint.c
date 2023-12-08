70e7a28130a90c212405cba6c14aa4220b73b32a
Ogunyemi Daniel <ogunyemidaniel65@gmail.com>
2023-12-08 07:29:29 -0800

    C-double

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -19,7 +19,7 @@ size_t print_dlistint(const dlistint_t *h)
 		return (0);
 	}
 	count = 0;
-	while (h)
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		count++;
---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * add_dnodeint - A function that adds a new node at beginning of a list
+ * @head: A pointer.
+ * @n: An integer.
+ * Return: The address of the new element, or NULL if it failed.
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	new->n = n;
+	new->prev = NULL;
+	if (*head == NULL)
+	{
+		*head = new;
+	}
+	new->next = *head;
+	*head = new;
+	if (*head == NULL)
+	{
+		return (NULL);
+	}
+	return (*head);
+}
