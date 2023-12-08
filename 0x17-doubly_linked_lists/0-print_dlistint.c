d15ae71a1104d2445ee1cd4e817fa812e403b577
Ogunyemi Daniel <ogunyemidaniel65@gmail.com>
2023-12-08 05:22:23 -0800

    C-double

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_dlistint - A function that prints all the
+ * elements of a dlistint_t list.
+ * @h: A pointer.
+ * Return: The  number of nodes.
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	int count;
+	
+	if (h == NULL)
+	{
+		return (0);
+	}
+	count = 0;
+	while (h)
+	{
+		printf("%d\n", h->n);
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
