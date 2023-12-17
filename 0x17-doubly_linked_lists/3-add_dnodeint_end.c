490bb6cd3039ceabc49443f58b895e3e3d60a90d
Ogunyemi Daniel <ogunyemidaniel65@gmail.com>
2023-12-17 07:04:01 -0800

    C-double

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -22,10 +22,10 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	else
 	{
 		new->n = n;
+		new->prev = NULL;
 		new->next = NULL;
 		if (*head == NULL)
 		{
-			new->prev = NULL;
 			*head = tmp = new;
 		}
 		tmp->next = new;
