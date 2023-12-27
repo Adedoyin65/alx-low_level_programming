da25c1fe3ba85fb9846abe2cf175d7f43db3ce04
Ogunyemi Daniel <ogunyemidaniel65@gmail.com>
2023-12-27 02:40:05 -0800

    Hash

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,9 +13,16 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new;
 
-	new = malloc(sizeof(size));
+	new = malloc(sizeof(hash_table_t));
 	if (new == NULL)
 	{
+		return (NULL);
+	}
+	new->size = size;
+	new->array = malloc(sizeof(hash_node_t *) * size);
+	if (new->array == NULL)
+	{
+		free(new);
 		return (NULL);
 	}
 	return(new);
