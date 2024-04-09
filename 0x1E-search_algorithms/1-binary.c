0f54c5b8bcc95a194679785b25375b40fb5f4abb
Ogunyemi Daniel <ogunyemidaniel65@gmail.com>
2024-04-08 23:41:55 -0700

    The time complexity (worst case) of a linear search in an array of size n

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -26,7 +26,7 @@ int binary_search(int *array, size_t size, int value)
 		printf("Searching in array: ");
 		for (i = left; i <= right; ++i)
 		{
-			printf("%d ", array[i]);
+			printf("%d, ", array[i]);
 		}
 		printf("\n");
 
