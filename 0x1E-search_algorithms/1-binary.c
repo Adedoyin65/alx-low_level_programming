53a1fda805a84d9a11ef411c3c54b0bb3575a897
Ogunyemi Daniel <ogunyemidaniel65@gmail.com>
2024-04-11 12:23:58 -0700

    A function that searches for a value in a sorted array of integers using the Binary search algorithm

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,48 +1,49 @@
 #include <stdio.h>
 #include <math.h>
-/**
- * binary_search - A function that searches for a value in an array
- * of integers using the Linear search algorithm
- * @array: A pointer to the array
- * @size: The size of the array
- * @value: The value to be found
- * Return: The index of value
- */
+
 int binary_search(int *array, size_t size, int value)
 {
-	size_t left, right, mid, i;
+        size_t i, left, right, mid;
 
-	if (array == NULL)
-	{
-		return (-1);
-	}
-	left = 0;
-	right = size - 1;
-	mid = (int)floor((left + right) / 2);
-	printf("Searching in array: ");
-	for (i = left; i <= right; i++)
-	{
-		if (i == right)
-		{
-			printf("%d ", array[i]);
-		}
-		else
-		{
-			printf("%d, ", array[i]);
-		}
-	}
-	printf("\n");
-	if (array[mid] == value)
-	{
-		return (mid);
-	}
-	else if (array[mid] < value)
-	{
-		left = mid + 1;
-	}
-	else
-	{
-		right = mid - 1;
-	}
-	return (-1);
+        if (array == NULL)
+        {
+                printf("Array is NULL\n");
+                return -1;
+        }
+
+        left = 0;
+        right = size - 1;
+
+        while (left <= right)
+        {
+                printf("Searching in subarray: ");
+                for (i = left; i <= right; ++i)
+                {
+                        if (i == right)
+			{
+				printf("%d ", array[i]);
+			}
+			else
+			{
+				printf("%d, ", array[i]);
+			}
+                }
+                printf("\n");
+
+                mid = (int)floor(left + (right - left) / 2);
+
+                if (array[mid] == value)
+                {
+                        return mid;
+                }
+                else if (array[mid] < value)
+                {
+                        left = mid + 1;
+                }
+                else
+                {
+                        right = mid - 1;
+                }
+        }
+        return -1;
 }
